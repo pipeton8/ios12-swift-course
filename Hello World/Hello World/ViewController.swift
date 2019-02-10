52527658b8891c243fdcdb2719ed79a5680fb34a
Felipe Del Canto <felipe.delcanto@icloud.com>
2019-02-09 22:25:22 -0300

    Created "Hello World!" App

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Felipe Del Canto Monge on 2/9/19.
+//  Copyright © 2019 Felipe Del Canto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
