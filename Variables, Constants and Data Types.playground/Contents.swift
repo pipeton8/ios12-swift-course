9062d1114fb98392ff7c0e2a26c25611d37bc5f8
Felipe Del Canto <felipe.delcanto@icloud.com>
2019-02-12 13:42:46 -0300

    Variables, Constants and Data Types

---
+++ Variables, Constants and Data Types.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var myAge : Int = 26
+let myName : String = "Felipe"
+let mySurName : String = "Del Canto"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = myName + " Del Canto"
+let myFullName2 = "\(myName) \(mySurName)"
+
+let myDetails = "\(myFullName), \(myAge)"
