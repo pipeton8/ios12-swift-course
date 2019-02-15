333d3ad29b778a127654ece7f1280285b5125f24
Felipe Del Canto <felipe.delcanto@icloud.com>
2019-02-15 15:36:07 -0300

    Practicing Auto Layout

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Felipe Del Canto Monge on 2/15/19.
+//  Copyright © 2019 Felipe Del Canto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
